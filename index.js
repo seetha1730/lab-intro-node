4a59dd5c87c6d753eb91c23e84f0463ecadbcc01
“seetha1730” <seetharaj1990@gmail.com>
2023-08-11 16:08:07 +0200

    lab intro node

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+    this.arraySum = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.includes(pos)) {
+      return this.items.indexOf(pos);
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length > 0) {
+      return this.items.sort((a, b) => b - a)[0];
+    } else {
+      throw new Error("Empty SortedList");
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length > 0) {
+      return this.items.sort()[0];
+    } else {
+      throw new Error("Empty SortedList");
+    }
+  }
 
-  sum() {}
+  sum() {
+    for (let i = 0; i < this.items.length; i++) this.arraySum += this.items[i];
+    return this.arraySum;
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
